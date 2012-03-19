4280539c90049a7f49d3dcd8d5761471304a8315
unknown <garcia amadeo@garciaamadeo-PC.(none)>
2012-03-19 18:27:19 -0300

    subiendo 1º vista

---
+++ mivista.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>
+            <?= $titulo ?>
+        </title>
+    </head>
+    <body>
+        <?php echo $cont;
+        ?>
+    </body>
+<?php
+	echo "<h1 align='center'>Mirá desde mi vista!</h1>";
+?>
+</html>
